5f282dda727c13ac9bf142da96de8f53b823644a
Blandus1 <blandine0045@gmail.com>
2026-03-14 17:44:25 +0200

    Aced the sixth challenge yeahhhh!

---
+++ task6.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function formatMessage(message) {
+    switch (message.type) {
+        case "text":
+            return ("Text: " + message.content);
+            break;
+        case "image":
+            return ("Image [" + message.caption + "]: " + message.url || "Image : " + message.url);
+            break;
+        case "video": return ("Video (" + message.duration + "s): " + message.url);
+        default: return ("Operation failed.");
+    }
+}
+console.log(formatMessage({ type: "text", content: "Hello, world!" }));
+console.log(formatMessage({ type: "image", url: "<https://example.com/image.jpg>", caption: "Sunset" }));
+console.log(formatMessage({ type: "video", url: "<https://example.com/video.mp4>", duration: 120 }));
+//# sourceMappingURL=task6.js.map
---
+++ task6.ts
@@ -0,0 +1,36 @@
+//sixth challenge
+interface TextMessage{
+    type: "text";
+    content: string;
+}
+
+interface ImageMessage {
+    type: "image"
+    url: string;
+    caption ?:string;
+}
+
+interface VideoMessage{
+    type: "video";
+    url: string;
+    duration:number;
+}
+
+
+type ChatMessage = TextMessage | ImageMessage | VideoMessage;
+
+function formatMessage (message: ChatMessage): string{
+switch (message.type){
+    case "text":return("Text: " + message.content);
+    break;
+    case "image": return("Image [" + message.caption + "]: " + message.url || "Image : " + message.url);
+    break;
+    case "video": return ("Video (" + message.duration + "s): " + message.url);
+    default: return("Operation failed.")
+}
+
+}
+
+console.log(formatMessage({type: "text", content: "Hello, world!"}))
+console.log(formatMessage({type: "image",url: "<https://example.com/image.jpg>", caption: "Sunset" }))
+console.log(formatMessage({type: "video",  url: "<https://example.com/video.mp4>", duration: 120}))
